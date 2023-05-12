ea105056dfcc2ef68061e228521d7ab3e45996a8
Banjo Kavanagh <tyranids1234@gmail.com>
2023-05-12 12:44:06 +1000

    QOL // Cleaning
    Added the back button which allows the user to go back to the main menu screen, also removed // refactored the old code so it works with my new system of window changing.

--- difficulty_selection.py
+++ difficulty_selection.py
@@ -5,10 +5,12 @@ import sys
 from databasemanager import DatabaseManager
 
 class DifficultySelection(QMainWindow):
-    def __init__(self, currentWindow, pages_dict, account_information):
+    def __init__(self, currentWindow, pages_dict, account_information, game):
         super().__init__()
         
         self.show_best_times = False
+        self.game = game
+        self.login = False
         
         self.currentWindow = currentWindow
         self.pages_dict = pages_dict
@@ -26,25 +28,32 @@ class DifficultySelection(QMainWindow):
         self.setFixedSize(618, 500)
         
         self.difficulty_dict = {"Easy": 0.5, "Medium": 0.6, "Hard": 0.7, "Expert": 0.8}
+        self.difficulty_num = {"Easy": 0, "Medium": 1, "Hard": 2, "Expert": 3}
         
         self.button_posX = 109
         self.button_posY = 150 
         
+        self.back_button = QPushButton('⬅', self)
+        self.back_button.setFont(QFont('Arial', 20))
+        self.back_button.setGeometry(10, 5, 35, 42)
+        self.back_button.clicked.connect(self.back)
+        
+        
         self.easy_button = QPushButton("Easy", self)
         self.easy_button.setGeometry(self.button_posX, self.button_posY, 200, 40)
-        self.easy_button.clicked.connect(self.easyDifficulty)
+        self.easy_button.clicked.connect(lambda: self.choose_difficulty('Easy'))
         
         self.medium_button = QPushButton("Medium", self)
         self.medium_button.setGeometry(self.button_posX, self.button_posY+55, 200, 40)
-        self.medium_button.clicked.connect(self.mediumDifficulty)
+        self.medium_button.clicked.connect(lambda: self.choose_difficulty('Medium'))
         
         self.hard_button = QPushButton("Hard", self)
         self.hard_button.setGeometry(self.button_posX, self.button_posY+110, 200, 40)
-        self.hard_button.clicked.connect(self.hardDifficulty)
+        self.hard_button.clicked.connect(lambda: self.choose_difficulty('Hard'))
         
         self.expert_button = QPushButton("Expert", self)
         self.expert_button.setGeometry(self.button_posX, self.button_posY+165, 200, 40)
-        self.expert_button.clicked.connect(self.expertDifficulty)
+        self.expert_button.clicked.connect(lambda: self.choose_difficulty('Expert'))
         
         self.choose_difficulty_label = QLabel("         Difficulty         ", self) # 9 Spaces on each side to center the text and add an underline effect.
         font = QFont()
@@ -67,10 +76,12 @@ class DifficultySelection(QMainWindow):
         
         
             
-            
+    def back(self):
+        self.currentWindow.setCurrentWidget(self.pages_dict["Main Menu"])
         
     def update(self):
         if self.show_best_times:
+            self.login = True
             self.load_times = self.account_information[2]
             self.easy = self.load_times[0]
             self.medium = self.load_times[1]
@@ -95,19 +106,12 @@ class DifficultySelection(QMainWindow):
             
             self.locked_box.setStyleSheet("background-color: rgba(0, 0, 0, 0)")
             self.locked_text.hide()
-        
-    def easyDifficulty(self):
-        print(f"Difficulty Easy chosen with a value of {self.difficulty_dict['Easy']}")
-    
-    def mediumDifficulty(self):
-        print(f"Difficulty Medium chosen with a value of {self.difficulty_dict['Medium']}")
-    
-    def hardDifficulty(self):
-        print(f"Difficulty Hard chosen with a value of {self.difficulty_dict['Hard']}")
-    
-    def expertDifficulty(self):
-        print(f"Difficulty Expert chosen with a value of {self.difficulty_dict['Expert']}")
-        
+            
+    def choose_difficulty(self, difficulty):
+        self.game.setup_board([self.difficulty_dict[difficulty], difficulty])
+        if self.login:
+            self.game.personal_best_time.setText(self.load_times[self.difficulty_num[difficulty]])
+        self.currentWindow.setCurrentWidget(self.pages_dict['Game Menu'])
         
 if __name__ == '__main__':
     
